ad63304dc1adbdc0edf45ef3555ab0b9a7c873bd
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-04-21 20:35:43 -0400

    Added Camera Control

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,19 +10,27 @@ public class Player : MonoBehaviour
     private Vector3 _direction;
     private Vector3 _velocity;
     private CharacterController _controller;
+    private Camera _cam;
 
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _cam = Camera.main;
 
         if (_controller == null)
         {
             Debug.LogError("The CHARACTER CONTROLLER is NULL");
         }
+
+        if (_cam == null)
+        {
+            Debug.LogError("The MAIN CAMERA is NULL");
+        }
     }
 
     private void Update()
     {
+        CameraRotation();
         Movement();
     }
 
@@ -42,7 +50,23 @@ public class Player : MonoBehaviour
             }
         }
 
+        _velocity = transform.TransformDirection(_velocity);
+
         _velocity.y -= _gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
+    }
+
+    void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 currentRotation = transform.localEulerAngles;
+        currentRotation.y += mouseX;
+        transform.localRotation = Quaternion.AngleAxis(currentRotation.y, Vector3.up);
+
+        Vector3 currentCamRotation = _cam.gameObject.transform.localEulerAngles;
+        currentCamRotation.x -= mouseY;
+        _cam.gameObject.transform.localRotation = Quaternion.AngleAxis(currentCamRotation.x, Vector3.right);
     }
 }
