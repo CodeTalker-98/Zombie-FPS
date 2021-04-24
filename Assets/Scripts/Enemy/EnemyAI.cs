ddef103f73ee888360136608f78dee14eb55e53a
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-04-24 13:31:19 -0400

    Enemy can walk/Find Player

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private float _spd = 2.0f;
+    [SerializeField] private float _gravity = 9.81f;
+    private Vector3 _velocity;
+    private Transform _player;
+    private CharacterController _controller;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        if (_controller == null)
+        {
+            Debug.LogError("The CHARACTER CONTROLLER is NULL");
+        }
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (_controller.isGrounded)
+        {
+            Vector3 direction = _player.position - transform.position;
+            direction.Normalize();
+            direction.y = 0;
+            transform.localRotation = Quaternion.LookRotation(direction);
+            _velocity = direction * _spd;
+        }
+
+        _velocity.y -= _gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
+    }
+}
