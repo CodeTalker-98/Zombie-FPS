b7492ba18c3d84232524238a25b76ed915fd53eb
Jim Gagliardi <jvgagliardi98@outlook.com>
2021-04-28 19:48:03 -0400

    Working on IK Rigging

---
+++ Assets/Scripts/SmoothDamp.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothDamp : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private float _spd = 10.0f;
+
+    private void LateUpdate()
+    {
+        Vector3 targetPosition = _target.transform.position;
+        transform.position = Vector3.Lerp(transform.position, targetPosition, _spd * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(_target.transform.rotation.eulerAngles);
+    }
+}
